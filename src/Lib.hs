196c91f42acf8e34529ed5438195802e39c693cf
Levisoto <levisoto.337@gmail.com>
2018-01-18 17:00:51 -0500

    I completed eighth week

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,9 +3,19 @@ module Lib where
 
 import Employee
 import Data.Tree
+import Data.List
 
+-- Problem 5 : make IO() part
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = readFile "company.txt" >>= (putStrLn.format.maxFun.read)
+
+format :: GuestList -> String
+format (GL emp fun) = m ++ (unlines n) 
+  where
+    m = "Total fun: " ++ show fun ++"\n"
+    -- n = map (\x -> empName x) (sortBy compare emp)
+    n = sortBy compare (map (\x -> empName x) emp)
+    
 
 glCons :: Employee -> GuestList -> GuestList
 glCons emp (GL list fun) = (GL (emp:list) ((empFun emp)+fun))
@@ -35,3 +45,13 @@ nextLevel emp list = (nx,ny)
     m = (map (glCons emp) wIth)
     nx = foldl moreFun mempty m
     ny = foldl moreFun mempty wOut
+
+-- Problem 4 : I need to find two best guest list: One with the boss and another without him
+maxFun :: Tree Employee -> GuestList
+maxFun tree = (\(x,y)->max x y) $ treeFold nextLevel tree
+-- maxFun  (Node a xs) =  
+--   where
+--     m = nextLevel a (createTwo xs)
+    
+-- createTwo (Node m []) = [glCons m mempty,gl m mempty]
+-- createTwo (Node m xs) = map (\y -> (m,glCons m y)) xs
