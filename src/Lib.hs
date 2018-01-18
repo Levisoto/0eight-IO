a2105994f4e00ac94488dc2889058dbc40388f55
Levisoto <levisoto.337@gmail.com>
2018-01-18 11:35:05 -0500

    I completed the problem 3

--- src/Lib.hs
+++ src/Lib.hs
@@ -19,7 +19,19 @@ moreFun gl1@(GL _ fun1) gl2@(GL _ fun2)
   | fun1>fun2 = gl1
   | otherwise = gl2
 
+-- Problem 2
 treeFold :: (a -> [b] -> b) -> Tree a -> b
 treeFold f = go where
     go (Node x ts) = f x (map go ts)
 -- treeFold f i tree = (foldl (f (rootLabel tree)) i (subForest tree))
+--
+--
+--
+-- Problem 3 : I need to find two best guest list: One with the boss and another without him
+nextLevel :: Employee -> [(GuestList,GuestList)] -> (GuestList,GuestList)
+nextLevel emp list = (nx,ny)
+  where 
+    (wIth,wOut) = unzip list
+    m = (map (glCons emp) wIth)
+    nx = foldl moreFun mempty m
+    ny = foldl moreFun mempty wOut
